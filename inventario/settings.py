ee1d82462cd54baf4461f923b24d1ed9971afdaa
johaans <johanse1996@hotmail.com>
2020-07-21 09:03:52 -0500

    second commit

--- inventario/settings.py
+++ inventario/settings.py
@@ -83,8 +83,14 @@ WSGI_APPLICATION = 'inventario.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'inventarioing$default',
+        'OPTIONS':{'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
+        'USER': 'inventarioing',
+        'PASSWORD': 'Itm733922',
+        'HOST':'inventarioing.mysql.pythonanywhere-services.com',
     }
 }
 
