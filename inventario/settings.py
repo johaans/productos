ba8aaaf21d9d55673875e98c810ccda2c65250bd
johaans <johanse1996@hotmail.com>
2020-07-22 09:33:21 -0500

    otro

--- inventario/settings.py
+++ inventario/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'inventarioing$default',
+        'NAME': 'inventarioing$inventario',
         'OPTIONS':{'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
         'USER': 'inventarioing',
         'PASSWORD': 'Itm733922',
